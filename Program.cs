efdc467ea3e623a1036c0dd291a855dd2cc9b43d
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-08 11:01:51 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace AddressBookServiceDB
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Service using DataBase");
+            Console.ReadLine();
+        }
+    }
+}
